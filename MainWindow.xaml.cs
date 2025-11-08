f06561312685b406224fb74f483b5140b437ce22
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2025-11-08 20:40:43 +0300

    Версия 1. Калькулятор работает

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,44 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void Oper(object sender, RoutedEventArgs e)
+        {
+            if(!double.TryParse(op1.Text, out double a)) return;
+            if(!double.TryParse(op2.Text, out double b)) return;
+
+            string op = (sender as Button)?.Name;
+            double result = op switch
+            {
+                "add" => a + b,
+                "sub" => a - b,
+                "mul" => a * b,
+                "div" => b != 0 ? a / b : double.NaN,
+                _ => 0
+            };
+            res.Text = result.ToString();
+        }
+
+        void Clear(object sender, RoutedEventArgs e)
+        {
+            op1.Text = op2.Text = res.Text = "0";
+        }
+
+        void CopyResult(object sender, RoutedEventArgs e)
+        {
+            
+            op1.Text = res.Text;
+            op2.Text = "0";
+            res.Text = "0";
         }
     }
 }
