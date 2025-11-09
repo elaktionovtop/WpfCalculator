636a6afddb2b3586c41975c7ec816773dd6d8cab
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2025-11-09 21:14:37 +0300

    Версия 4. Использование команд

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,28 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Calculator
 {
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private double operand1;
-        private double operand2;
+        enum Op { Add, Sub, Mul, Div }
+
+        private string operand1;
+        private string operand2;
         private double result;
 
-        public double Operand1
+        public string Operand1
         {
             get => operand1;
-            set { operand1 = value; OnPropertyChanged(); }
+            set { operand1 = value; OnPropertyChanged(); NotifyCommands(); }
         }
 
-        public double Operand2
+        public string Operand2
         {
             get => operand2;
-            set { operand2 = value; OnPropertyChanged(); }
+            set { operand2 = value; OnPropertyChanged(); NotifyCommands(); }
         }
 
         public double Result
@@ -28,30 +31,93 @@ namespace Calculator
             set { result = value; OnPropertyChanged(); }
         }
 
+        // Команды
+        public RelayCommand AddCommand { get; }
+        public RelayCommand SubCommand { get; }
+        public RelayCommand MulCommand { get; }
+        public RelayCommand DivCommand { get; }
+        public RelayCommand NextCommand { get; }
+        public RelayCommand ClearCommand { get; }
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
+
+            AddCommand = new RelayCommand(_ => ExecuteOper(Op.Add), _ => CanExecuteBinary());
+            SubCommand = new RelayCommand(_ => ExecuteOper(Op.Sub), _ => CanExecuteBinary());
+            MulCommand = new RelayCommand(_ => ExecuteOper(Op.Mul), _ => CanExecuteBinary());
+            DivCommand = new RelayCommand(_ => ExecuteOper(Op.Div), _ => CanExecuteBinary(divCheck: true));
+            NextCommand = new RelayCommand(_ => CopyResult(), _ => true);
+            ClearCommand = new RelayCommand(_ => Clear(), _ => true);
         }
 
-        void Oper(object sender, RoutedEventArgs e)
+        void ExecuteOper(Op op)
         {
-            var name = (sender as FrameworkElement)?.Name;
-            Result = name switch
+            if(!TryGetOperands(out var a, out var b, out var err))
             {
-                "add" => Operand1 + Operand2,
-                "sub" => Operand1 - Operand2,
-                "mul" => Operand1 * Operand2,
-                "div" => Operand2 != 0 ? Operand1 / Operand2 : double.NaN,
+                MessageBox.Show(err, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            double res = op switch
+            {
+                Op.Add => a + b,
+                Op.Sub => a - b,
+                Op.Mul => a * b,
+                Op.Div => b != 0 ? a / b : double.NaN,
                 _ => Result
             };
+
+            Result = Math.Round(res, 2);
         }
 
-        void CopyResult(object sender, RoutedEventArgs e) => Operand1 = Result;
-        void Clear(object sender, RoutedEventArgs e) => Operand1 = Operand2 = Result = 0;
+        // Централизованная проверка и выполнение
+        bool TryGetOperands(out double a, out double b, out string error)
+        {
+            a = b = 0;
+            error = string.Empty;
+            if(!double.TryParse(Operand1, out a))
+            {
+                error = "Ошибка ввода: первое число";
+                return false;
+            }
+            if(!double.TryParse(Operand2, out b))
+            {
+                error = "Ошибка ввода: второе число";
+                return false;
+            }
+            return true;
+        }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-        void OnPropertyChanged([CallerMemberName] string? prop = null)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        bool CanExecuteBinary(bool divCheck = false)
+        {
+            if(!double.TryParse(Operand1, out var a)) return false;
+            if(!double.TryParse(Operand2, out var b)) return false;
+            if(divCheck && b == 0) return false;
+            return true;
+        }
+
+        void CopyResult() => Operand1 = Result.ToString();
+        void Clear()
+        {
+            Operand1 = Operand2 = string.Empty;
+            Result = double.NaN;
+        }
+
+        // Уведомляем команды, что вход изменился
+        void NotifyCommands()
+        {
+            AddCommand.RaiseCanExecuteChanged();
+            SubCommand.RaiseCanExecuteChanged();
+            MulCommand.RaiseCanExecuteChanged();
+            DivCommand.RaiseCanExecuteChanged();
+            NextCommand.RaiseCanExecuteChanged();
+            ClearCommand.RaiseCanExecuteChanged();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string? prop = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
     }
 }
---
+++ RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object?> execute;
+        private readonly Func<object?, bool>? canExecute;
+        public RelayCommand(Action<object?> exec, Func<object?, bool>? can = null)
+        { execute = exec; canExecute = can; }
+
+        public bool CanExecute(object? p) => canExecute?.Invoke(p) ?? true;
+        public void Execute(object? p) => execute(p);
+        public event EventHandler? CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
